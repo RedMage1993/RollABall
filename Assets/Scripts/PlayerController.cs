5a5b816916be61ecd7eeed90ef5e3a732dc0a082
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-06-02 22:30:16 -0700

    Started coding PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    
+    // Check every frame for player input
+    // Apply player input every frame as movement
+    // Check for input on update and fixedUpdate
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Where most of the game code will go
+    //void Update()
+    //{
+
+    //}
+
+    // Called just before performing any physics calculations
+    // Where physics code will go
+    void FixedUpdate()
+    {
+        // Can Ctrl + ' on Unity editor to bring up documentation
+        // GetAxis grabs input from player through the keyboard
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Use Input to apply force to rigidbody of Player
+        // TODO: Continue tut at 7:40
+    }
 }
